a0acf7d5d7bec61ea5dd03fe30e33f933abbaf11
Baday19 <1556330234@qq.com>
2025-07-20 01:47:20 +0800

    build

---
+++ build/data.js
@@ -0,0 +1,38 @@
+var _full = [
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: 'zju', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: 'zju', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+]
+var _verified = [
+  { method: "Deng", model: 99, resolved: 10, org: 99, site: 'zju', date: '2023-04-03' },
+  { method: "Deng", model: 99, resolved: 10, org: 99, site: 'zju', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+  { method: "Huang", model: 99, resolved: 10, org: 99, site: '', date: '2023-04-03' },
+]
+
+var _leaderboard = [
+  {name: 'FULL', data: _full},
+  {name: 'Verified', data: _verified},
+]
+
+  // public/data.js
+  window.__APP_DATA__ = {
+    leaderboard: _leaderboard,
+  }
